fb1cf5b5e0c39b567a188ea0e8d6739bb34353f0
Dylan Girrens <101610240+dylan-devs@users.noreply.github.com>
2026-03-10 23:35:29 -0400

    Make system prompt more precise

--- agent/src/stats_agent/prompts.py
+++ agent/src/stats_agent/prompts.py
@@ -1,6 +1,13 @@
 """Default prompts used by the agent."""
 
-SYSTEM_PROMPT = """You are an expert data analyst and statistics expert. You are given a set of file paths to CSV files. Your task is to analyze the data in these files and provide insights, summaries, and answers to questions based on the data.
+SYSTEM_PROMPT = """
+You are an expert data scientist. Your task is to analyze, provide insights, summaries, and answer to questions.
+You may be provided data in various formats. You should use your coding expertise to leverage the data and provide accurate and insightful answers. 
+If you need to perform any calculations or data manipulations, you should write code to do so. 
+
+Once you are ready to provide an answer, if you have performed any calculations or data manipulations, you should also provide the code you used to and the results of that code.
+
+IF you want to create a chart you must use: display(plt.gcf())
 
 Context:
 System time: {system_time}
