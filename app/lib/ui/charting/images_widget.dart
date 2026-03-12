47c3ca2fa86eff73c24fbd06236736390775bdf0
Dylan Girrens <101610240+dylan-devs@users.noreply.github.com>
2026-03-12 19:19:15 -0400

    add scrolling to images widget

--- app/lib/ui/charting/images_widget.dart
+++ app/lib/ui/charting/images_widget.dart
@@ -1,30 +1,51 @@
 import 'dart:typed_data';
-
 import 'package:flutter/material.dart';
-
 import 'image_widget.dart';
 
-class ImagesWidget extends StatelessWidget {
+class ImagesWidget extends StatefulWidget {
   const ImagesWidget({super.key, required this.images});
 
   final List<Uint8List> images;
 
   @override
+  State<ImagesWidget> createState() => _ImagesWidgetState();
+}
+
+class _ImagesWidgetState extends State<ImagesWidget> {
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    if (images.isNotEmpty) {
-        return Container(
-          width: double.infinity,
-          padding: const EdgeInsets.all(12),
+    if (widget.images.isEmpty) {
+      return const SizedBox.shrink();
+    }
+
+    return SizedBox(
+      height: 380,
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Scrollbar(
+          controller: _scrollController,
+          thumbVisibility: true,
+          interactive: true,
           child: SingleChildScrollView(
+            controller: _scrollController,
             scrollDirection: Axis.horizontal,
             child: Row(
-              children: 
-                images.map((b) => ImageWidget(bytes: b)).toList(),
-            )
-          ));
-      }
-      else {
-        return SizedBox.shrink();
-      }
-    }
+              children: widget.images
+                  .map((b) => ImageWidget(bytes: b))
+                  .toList(),
+            ),
+          ),
+        ),
+      ),
+    );
   }
+}
